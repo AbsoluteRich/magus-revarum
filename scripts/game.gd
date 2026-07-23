65560f76b2425b987e9bee3f33cae330d69e631b
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-07-23 12:37:18 +1000

    Spell chain logic

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,17 +1,23 @@
 extends Control
 
 @onready var timer: Timer = $TurnTimer
-@onready var timer_bar: ProgressBar = $VBoxContainer/TimerBar
+@onready var timer_bar: ProgressBar = $Hud/TimerBar
 @onready var enemy: VBoxContainer = $Enemy
+@onready var spell_chain_container: HBoxContainer = $SpellChain
+@onready var form: ButtonGroup = preload("res://scenes/forms.tres")
 
 var initial_time: float = 5
-var spell_chain: Array[String] = []
+var spell_chain: Array[String] = [""]
 
 
 func _ready() -> void:
 	timer.wait_time = initial_time
 	timer_bar.max_value = initial_time
 	timer.start()
+	
+	for element in spell_chain_container.get_children():
+		element.connect("pressed", handle_rune.bind(element))
+
 
 
 func _process(_dt: float) -> void:
@@ -22,3 +28,14 @@ func _process(_dt: float) -> void:
 
 func _on_attack_pressed() -> void:
 	enemy.update_health(-5)
+
+
+func handle_rune(rune: Button):
+	if rune.button_group == form:
+		spell_chain[0] = rune.text
+	else:
+		if rune.text in spell_chain:
+			spell_chain.remove_at(spell_chain.find(rune.text))
+		else:
+			spell_chain.append(rune.text)
+	print(spell_chain)
